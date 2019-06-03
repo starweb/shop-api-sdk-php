964ad78f8c44a8480f054ac0c33d5ada1f587206
Patrick Rodacker <patrick@rodacker.de>
2019-06-03 08:25:55 +0200

    fixes signature for createMediaFile and throw an error if the object is not an instance of the inherited MediaFileUploadModel class

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -3,12 +3,18 @@
 namespace Starweb\Api;
 
 use Starweb\Api\Generated\Client as BaseClient;
-use Starweb\Api\Model\MediaFileUploadModel;
+use Starweb\Api\Generated\Model\MediaFileUploadModel;
 
 class Client extends BaseClient
 {
     public function createMediaFile(MediaFileUploadModel $requestBody = null, string $fetch = BaseClient::FETCH_OBJECT)
     {
+        if (!$requestBody instanceof \Starweb\Api\Model\MediaFileUploadModel) {
+            throw new \LogicException(
+                'you need to pass an object of type ' . \Starweb\Api\Model\MediaFileUploadModel::class
+            );
+        }
+
         return $this->executePsr7Endpoint(new \Starweb\Api\Endpoint\CreateMediaFile($requestBody), $fetch);
     }
 }
